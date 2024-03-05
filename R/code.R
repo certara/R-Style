772ddeba27c200df40a5523ac158560521fe11b6
certara-jcraig <61294078+certara-jcraig@users.noreply.github.com>
2024-03-04 19:32:03 -0800

    use assignment operator

--- R/code.R
+++ R/code.R
@@ -1,15 +1,15 @@
 
-certaraPNG = function() {system.file("extdata", "logo", "Certara_CB.png", package="certaracss")}
-styleCSS   = function() {system.file("extdata", "css",  "certara.css",    package="certaracss")}
-ICONDIR    = function() {system.file("extdata", "icons",                  package="certaracss")}
+certaraPNG <- function() {system.file("extdata", "logo", "Certara_CB.png", package="Certara.StyleR")}
+styleCSS   <- function() {system.file("extdata", "css",  "certara.css",    package="Certara.StyleR")}
+ICONDIR    <- function() {system.file("extdata", "icons",                  package="Certara.StyleR")}
 
 # return as raw HTML
-cat_raw_html = function(obj_) {
+cat_raw_html <- function(obj_) {
   cat(htmltools::HTML(as.character(obj_)))
 }
 
-add_certara_logo = function() {
-  img_ = htmltools::img(src   = knitr::image_uri(certaraPNG()),
+add_certara_logo <- function() {
+  img_ <- htmltools::img(src   = knitr::image_uri(certaraPNG()),
                         alt   = 'logo',
                         style = 'position:fixed; top:0; right:0; padding:10px; width:300px; z-index: -9;')
 
@@ -17,36 +17,36 @@ add_certara_logo = function() {
   cat_raw_html(img_)
 }
 
-grep_icondir_path = function(charvec,
+grep_icondir_path <- function(charvec,
                              special_dir="__ICONDIR__",
                              calculated_ICONDIR=ICONDIR()) {
   charvec = gsub(special_dir, calculated_ICONDIR, charvec)
   return(charvec)
 }
 
-load_certara_css = function() {
+load_certara_css <- function() {
   # load in css as character
-  stylesheet = readr::read_file(styleCSS())
+  stylesheet <- readr::read_file(styleCSS())
 
   # add appropriate tags to character vector
-  css_prefix  = '<style type="text/css">'
-  css_postfix = '</style>'
-  stylesheet = paste0(css_prefix,
+  css_prefix  <- '<style type="text/css">'
+  css_postfix <- '</style>'
+  stylesheet <- paste0(css_prefix,
                       stylesheet,
                       css_postfix)
 
   # grep ICONDIR placeholder to actual path on user system
-  stylesheet = grep_icondir_path(stylesheet)
+  stylesheet <- grep_icondir_path(stylesheet)
 
   # return as raw HTML
   cat_raw_html(stylesheet)
 }
 
-print_footer = function() {
-  text       = "" # insert text here to put at footer
-  prefix     = '<div style = "position:fixed; bottom: 0; right: 0; z-index: -9;"><p>'
-  postfix    = '</p></div>'
-  stylesheet = paste0(prefix,
+print_footer <- function() {
+  text       <- "" # insert text here to put at footer
+  prefix     <- '<div style = "position:fixed; bottom: 0; right: 0; z-index: -9;"><p>'
+  postfix    <- '</p></div>'
+  stylesheet <- paste0(prefix,
                       text,
                       postfix)
 
@@ -54,13 +54,13 @@ print_footer = function() {
   cat_raw_html(stylesheet)
 }
 
-activate_certara_style = function() {
+activate_certara_style <- function() {
   load_certara_css()
   add_certara_logo()
   print_footer()
 }
 
-activate_certara_style_if_not_interactive = function() {
+activate_certara_style_if_not_interactive <- function() {
   if (!interactive()) {
     activate_certara_style()
   }
